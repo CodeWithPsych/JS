2ec14949feedf4c2dd5cf8988b52dbc7f281fa86
Arham Malik <arhammalik900@yahoo.com>
2023-11-30 01:40:34 +0500

    Add files via upload

---
+++ console/2_operators.js
@@ -0,0 +1,41 @@
+// arithmetic oprators 
+let a = 23;
+let b = 45;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);    //exponentation
+console.log(a++);
+console.log(++a);
+console.log(b++);
+console.log(++b);
+console.log(a--);
+console.log(--a);
+console.log(b--);
+console.log(--b);
+
+// assigment operator:
+//                      =
+//                      +=
+//                      -=
+//                      *=
+//                      /=
+//                      %=
+//                      **=
+
+// comparison opertator :
+//                   ==
+//                   !=
+//                   ===
+//                   !==
+//                   >
+//                   <
+//                   >=
+//                   <=
+//                   ?
+
+// logical operators :
+//              &&  (and)
+//              ||  (or)
+//              !   (not)
