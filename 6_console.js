305480acc11043077e57cde6cf913747e26107d5
Arham Malik <arhammalik900@yahoo.com>
2023-11-30 01:39:04 +0500

    Delete 6_console.js

--- 6_console.js
+++
@@ -1,7 +0,0 @@
-console.error("I am a error");
-console.assert("5>6");
-let a =[1,23,45,66];
-console.table(a);
-// console.clear(); 
-console.warn("arham")
-console.table(console);   //print all consoles
